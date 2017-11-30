087461095be9d6fb9cfea9964f0f0406f0931b1a
clairelien <jl1993913@gmail.com>
2017-11-30 10:50:08 +0800

    first commit

---
+++ sorting.js
@@ -0,0 +1,8 @@
+module.exports = class algorithm{
+  constructor(){
+  }
+
+  test(){
+    console.log(123)
+  }
+}
---
+++ test.js
@@ -0,0 +1,6 @@
+var sorting = require('./sorting')
+
+var s = new sorting()
+
+console.log(s.test())
+
